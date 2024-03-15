39d9b70fb752269d73b03e5639cbb7ef2cbf1ac4
mishaster <mishablumchess@gmail.com>
2024-03-15 12:56:07 +0200

    Style Fixes + more precise time limit early folding

--- HW2/submission.py
+++ HW2/submission.py
@@ -31,7 +31,7 @@ def smart_heuristic(env: WarehouseEnv, robot_id: int):
     return a*robot.battery + b*robot.credit + c*calc_f_value(env, robot_id) + d*(robot.package is not None)
 
 class Node:
-    def __init__(self, env,operator, agent_id, parent=None, value = 0, children = [],max=True,depth =0) -> None:
+    def __init__(self, env , operator , agent_id, parent=None, value = 0, children = [] , max=True,depth =0) -> None:
         self.env = env
         self.operator = operator
         self.agent_id=agent_id
@@ -50,74 +50,72 @@ class AgentGreedyImproved(AgentGreedy):
 class AgentMinimax(Agent):
     # TODO: section b : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        ## TO HANDLE TIME LIMIT I SHOULD DO IT WITH ITERATIONS
         start = time.time()
         depth_for_minimax = 1
         magic_number_for_depth = 1000
-        root = Node(env,None,agent_id, None, 0, [],True,0)
+        root = Node(env, None , agent_id , None , 0, [] , True , 0 )
         curr_node = root
         max_node = True
-        root_node = self.create_tree_in_depth_j(root, 0)
-        for depth_for_minimax in range(1,magic_number_for_depth): #determines depth
-            root_node = self.create_tree_in_depth_j(root_node,depth_for_minimax)
-            #smart_heuristic(env,robot_id)
+        current_solution = None
+        root_node = self.create_tree_in_depth_j(root, 0, start, time_limit)
+        for depth_for_minimax in range(1, magic_number_for_depth): #determines depth
+            root_node = self.create_tree_in_depth_j(root_node, depth_for_minimax, start, time_limit)
+            if( root_node == None): # if folded, because of time shortage, return previous optimal solution
+                return current_solution
+            current_solution = root_node.policy.operator
             end = time.time()
-            if end - start > time_limit * 0.98:
-                return root_node.policy.operator
+
         raise NotImplementedError()
-    def choose_min(self):
-        raise NotImplementedError()
-    def choose_max(self):
-        raise NotImplementedError()
-    def create_tree_in_depth_j(self,root:Node,j:int):
+
+    def create_tree_in_depth_j(self, root:Node, j:int, start_time, time_limit):
+        if root == None:
+            return None
         if j == 0:
             root.value = smart_heuristic(root.env, root.agent_id)
             return root
+
         if j == 1 and len(root.children) == 0 : # node hasn`t been expanded yet
             successors = self.successors(root.env, root.agent_id)
             max_node = root.max
-            for i in range(len(successors[0])):  # succ is a list of paires of operators and the matching env
-                child_node = Node(successors[1][i],successors[0][i],root.agent_id^1, root, 0,[], (not root.max),root.depth+1)
-                child_node = self.create_tree_in_depth_j(child_node, j - 1)
+            for operator, child in zip(*successors):  # succ is a list of paires of operators and the matching env
+                if (time.time() - start_time > 0.9 * time_limit): # time is over, must fold
+                    return None
+                child_node = Node(child, operator, root.agent_id^1,
+                                  root, 0, [], (not root.max), root.depth+1)
+                child_node = self.create_tree_in_depth_j(child_node, j - 1, start_time, time_limit)
+                if(child_node == None):
+                    return None
                 root.children.append(child_node)
+
         else: # existing nodes
             for i in range(len(root.children)):
-                child = self.create_tree_in_depth_j(root.children[i],j-1)
+                if(time.time() - start_time > 0.9 *time_limit): # time is over, must fold
+                    return None
+                child = self.create_tree_in_depth_j(root.children[i], j-1, start_time, time_limit)
+                if(child == None):
+                    return None
                 root.children[i] = child
+
         root.policy = self.choose_policy(root)
         root.value = root.policy.value
         return root
-    def expand_tree_another_level(root_node:Node,depth_for_expansion=-1):
-        # Assumption that graph is balanced
-        curr_node = root_node
-        if len(curr_node.children) !=0 and curr_node.depth != depth_for_expansion:
-            successors = Agent.successors(curr_node.env, curr_node.agent_id)
-            for i in len(successors):  # succ is a list of paires of operators and the matching env
-                child_node = Node(successors[0][i], successors[1][i], curr_node.agent_id ^ 1, curr_node,
-                                  0, [], (not curr_node.max),
-                                  curr_node.depth + 1)
-                curr_node.append(child_node)
-            return curr_node
-        else: # expand till depth matches depth for expansion
-            for child in root_node.children: # need to replace existing child with new
-            # need to make sure, changes aren`t made on the copy
-                return NotImplementedError
+
     def choose_policy(self,root:Node):
         if(root.max == True):
             max = root.children[0].value
             policy_child = root.children[0]
-            for i in range(len(root.children)):
-                if root.children[i].value > max:
-                    max = root.children[i].value
-                    policy_child = root.children[i]
+            for child in root.children:
+                if child.value > max:
+                    max = child.value
+                    policy_child = child
             return policy_child
         else:
             min = root.children[0].value
             policy_child = root.children[0]
-            for i in range(len(root.children)):
-                if root.children[i].value < min:
-                    min = root.children[i].value
-                    policy_child = root.children[i]
+            for child in root.children:
+                if child.value < min:
+                    min = child.value
+                    policy_child = child
             return policy_child
 
 
